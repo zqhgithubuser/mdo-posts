30e0870241642efd76552fe34c0ffd0c6c609bb3
zqhgithubuser <szxw1209@gmail.com>
2024-06-27 17:01:04 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir -p /kaniko/.dockerconfig && ln -s /kaniko/.docker/.dockerconfigjson /kaniko/.dockerconfig/config.json
 IMAGE_ID=$1 && \
 IMAGE_TAG=$2 && \
 export DOCKER_CONFIG=/kaniko/.dockerconfig && \
